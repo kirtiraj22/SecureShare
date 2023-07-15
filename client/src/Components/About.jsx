c0d9aa0ba612cc7548efdb9a3ce5a7b1c5c03cf6
mohitparmar1 <contact.mohit06@gmail.com>
2023-07-15 21:52:55 +0530

    added about-us section

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -1,11 +1,21 @@
 import React from "react";
-
+import aboutImage from '../assets/aboutImage.png'
 const About = () => {
-  return (
-    <section id="home">
-      about
-    </section>
-  );
+    return (
+      <div className="flex flex-1 items-center justify-center p-0 m-0 w-full h-screen bg-gray-900">
+          <div className="w-1/2 flex flex-col justify-center px-3">
+            <h1 className="text-4xl font-bold mb-6 text-white">About Us</h1>
+            <p className="mb-4 text-white leading-loose">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed finibus elit, et dignissim tellus.
+              Mauris auctor scelerisque neque, vel maximus tellus malesuada eu. Integer tempor sagittis ligula, eget
+              fringilla massa efficitur ut.
+            </p>
+        </div>
+        <div className="flex w-25 mr-8">
+            <img src={aboutImage} alt="About Us" className="w-4/5 h-auto" />
+        </div>
+      </div>
+    );
 };
 
 export default About;
