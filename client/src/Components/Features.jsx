270211ba00aff07b3bffb2474beccf4a990b20aa
Kulrajsinh9904 <chauhankulrajsinh9904@gmail.com>
2023-07-15 22:40:59 +0530

    added a photo section

--- client/src/Components/Features.jsx
+++ client/src/Components/Features.jsx
@@ -1,7 +1,8 @@
 
-const Features = () => {
+
+const FeaturesSection = () => {
   return (
-    <div className="bg-gray-100 py-16">
+    <div className="bg-white py-16">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center">
           <h2 className="text-3xl font-extrabold text-gray-900">Features</h2>
@@ -11,63 +12,91 @@ const Features = () => {
         </div>
         <div className="mt-10 grid gap-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
           {/* Card 1 */}
-          <div className="bg-white overflow-hidden shadow rounded-lg hover:bg-gray-200 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+          <div className="bg-gray-200 overflow-hidden shadow rounded-lg hover:bg-gray-300 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
             <div className="p-6">
+              <img
+                src="path/to/feature1.jpg"
+                alt="Feature 1"
+                className="w-full h-40 object-cover mb-4 rounded-md"
+              />
               <h3 className="text-lg font-medium text-gray-900">Feature 1</h3>
-              
               <p className="mt-2 text-sm text-gray-500">
-              Feature 1: Decentralized storage. Your files are stored on a network of nodes, not on a single server. This makes them more secure and resistant to censorship. 
-
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               </p>
             </div>
           </div>
           
           {/* Card 2 */}
-          <div className="bg-white overflow-hidden shadow rounded-lg hover:bg-gray-200 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+          <div className="bg-gray-200 overflow-hidden shadow rounded-lg hover:bg-gray-300 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
             <div className="p-6">
+              <img
+                src="path/to/feature2.jpg"
+                alt="Feature 2"
+                className="w-full h-40 object-cover mb-4 rounded-md"
+              />
               <h3 className="text-lg font-medium text-gray-900">Feature 2</h3>
               <p className="mt-2 text-sm text-gray-500">
-              Feature 2: End-to-end encryption. Your files are encrypted before they are stored, so only you can access them. This ensures that your privacy is protected. 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               </p>
             </div>
           </div>
 
           {/* Card 3 */}
-          <div className="bg-white overflow-hidden shadow rounded-lg hover:bg-gray-200 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+          <div className="bg-gray-200 overflow-hidden shadow rounded-lg hover:bg-gray-300 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
             <div className="p-6">
+              <img
+                src="path/to/feature3.jpg"
+                alt="Feature 3"
+                className="w-full h-40 object-cover mb-4 rounded-md"
+              />
               <h3 className="text-lg font-medium text-gray-900">Feature 3</h3>
               <p className="mt-2 text-sm text-gray-500">
-              Feature 3: Scalability. The file system can scale to accommodate more users and files. This means that you can be confident that your files will always be accessible. 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               </p>
             </div>
           </div>
 
           {/* Card 4 */}
-          <div className="bg-white overflow-hidden shadow rounded-lg hover:bg-gray-200 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+          <div className="bg-gray-200 overflow-hidden shadow rounded-lg hover:bg-gray-300 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
             <div className="p-6">
+              <img
+                src="path/to/feature4.jpg"
+                alt="Feature 4"
+                className="w-full h-40 object-cover mb-4 rounded-md"
+              />
               <h3 className="text-lg font-medium text-gray-900">Feature 4</h3>
               <p className="mt-2 text-sm text-gray-500">
-              Feature 4: Cost-effectiveness. The file system is free to use and there are no monthly fees. This makes it a great option for budget-minded users. 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               </p>
             </div>
           </div>
 
           {/* Card 5 */}
-          <div className="bg-white overflow-hidden shadow rounded-lg hover:bg-gray-200 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+          <div className="bg-gray-200 overflow-hidden shadow rounded-lg hover:bg-gray-300 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
             <div className="p-6">
+              <img
+                src="path/to/feature5.jpg"
+                alt="Feature 5"
+                className="w-full h-40 object-cover mb-4 rounded-md"
+              />
               <h3 className="text-lg font-medium text-gray-900">Feature 5</h3>
               <p className="mt-2 text-sm text-gray-500">
-              Feature 5: User-friendly interface. The file system is easy to use and navigate. This makes it a great option for users of all levels of technical expertise. 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               </p>
             </div>
           </div>
 
           {/* Card 6 */}
-          <div className="bg-white overflow-hidden shadow rounded-lg hover:bg-gray-200 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+          <div className="bg-gray-200 overflow-hidden shadow rounded-lg hover:bg-gray-300 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
             <div className="p-6">
+              <img
+                src="path/to/feature6.jpg"
+                alt="Feature 6"
+                className="w-full h-40 object-cover mb-4 rounded-md"
+              />
               <h3 className="text-lg font-medium text-gray-900">Feature 6</h3>
               <p className="mt-2 text-sm text-gray-500">
-              Feature 6: Open source. The file system is open source, so you can be confident that it is secure and transparent. 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               </p>
             </div>
           </div>
@@ -77,5 +106,4 @@ const Features = () => {
   );
 };
 
-export default Features;
-
+export default FeaturesSection;
